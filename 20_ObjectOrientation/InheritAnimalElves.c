76e21264dc835ca9782f1d76d6249bdcc98990a0
Fernando Quintao Pereira <pronesto@gmail.com>
2024-12-12 08:22:03 -0300

    Adding elves/animal example.

--- 20_ObjectOrientation/InheritAnimalElves.c
+++ 20_ObjectOrientation/InheritAnimalElves.c
@@ -18,12 +18,11 @@ typedef struct Elf {
   void (*inc_age)(struct Animal* this);
   unsigned short (*get_age)(struct Animal* this);
 
-  char* tribe;
+  char* name;
 
   // More method pointers:
-  void (*set_name)(struct Elf* this);
-  unsigned short (*get_age)(struct Animal* this);
-} Animal;
+  void (*set_name)(struct Elf* this, char* name);
+} Elf;
 
 // Base "Animal" methods
 void animal_inc_age(Animal* this) {
@@ -40,6 +39,12 @@ void elf_inc_age(Animal* this) {
   this->age += 7;
 }
 
+// "Elf" methods
+void elf_set_name(Elf* this, char* name) {
+  // Well, elves live much longer...
+  this->name = name;
+}
+
 // Constructor for "Animal"
 Animal* new_animal(unsigned short age) {
   Animal* a = (Animal*)malloc(sizeof(Animal));
@@ -50,11 +55,13 @@ Animal* new_animal(unsigned short age) {
 }
 
 // Constructor for "Elf"
-Animal* new_elf(unsigned short age) {
-  Animal* a = (Animal*)malloc(sizeof(Animal));
+Elf* new_elf(unsigned short age, char* name) {
+  Elf* a = (Elf*)malloc(sizeof(Elf));
   a->age = age;
   a->inc_age = elf_inc_age;
   a->get_age = animal_get_age;
+  a->name = name;
+  a->set_name = elf_set_name;
   return a;
 }
 
@@ -66,16 +73,16 @@ int main() {
   printf("Animal's age after increment: %u\n", a->get_age(a));
 
   // Create an Elf
-  Animal* e = new_elf(100);
-  printf("Elf's initial age: %u\n", e->get_age((Animal*)e));
-  e->inc_age((Animal*)e);
-  printf("Elf's age after increment: %u\n", e->get_age((Animal*)e));
+  Animal* ae = (Animal*)new_elf(100, "Celebrimbor");
+  printf("Elf's initial age: %u\n", ae->get_age(ae));
+  ae->inc_age((Animal*)ae);
+  printf("Elf's age after increment: %u\n", ae->get_age(ae));
 
   // Create an Elf whose "static type" is Animal:
-  Animal* ae = (Animal*)new_elf(50, "Celebrimbor");
-  printf("Animal/elf's initial age: %u\n", ae->get_age(ae));
+  Elf* e = new_elf(50, "Celebrimbor");
+  printf("Animal/elf's initial age: %u\n", e->get_age(e));
   ae->inc_age(ae);
-  printf("Animal/elf's age after increment: %u\n", ae->get_age(ae));
+  printf("Animal/elf's age after increment: %u\n", e->get_age(e));
 
   // Cleanup
   free(a);
